dea455a0bf0845f396ed8756bdfd28e08e52b576
כרמל רון <caron1211@gmail.com>
2018-11-25 17:21:45 +0200

    first commit

---
+++ src/Coords/JTest.java
@@ -0,0 +1,57 @@
+package Coords;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.DecimalFormat;
+
+import org.junit.jupiter.api.Test;
+
+import Geom.Point3D;
+
+class JTest {
+
+	Point3D p = new Point3D(32.103315,35.209039,670); // 9
+	Point3D p1 = new Point3D(32.106352,35.205225,650); // houmos
+	Point3D v =  new Point3D(344,-400,0); // meter
+	@Test
+	void testAdd() {
+
+		MyCoords c = new MyCoords();
+		Point3D ans = c.add(p, v);
+		System.out.println(ans);
+		Point3D excepted = new Point3D(32.10640866632587,35.20599177041698,670.0);
+		assertTrue(excepted.equals(ans));
+	}
+
+	@Test
+	void testDistance()
+	{
+		MyCoords c = new MyCoords();
+		int actual = (int)c.distance3d(p, p1);
+		int excepted = 493;
+		System.out.println(actual);
+		assertEquals(excepted, actual);
+
+	}
+
+	@Test
+	void Lon_Norm() {
+		MyCoords c = new MyCoords();
+		double actual =c.Lon_Norm(p.x());
+		System.out.println("normal:"+actual);
+
+
+	}
+	
+	@Test
+	void  vector3D() {
+		MyCoords c = new MyCoords();
+		Point3D actual =c.vector3D(p,p1);
+		System.out.println(actual);
+		Point3D exceped = new Point3D(337.69899206128815,-359.24920693881893,-20.0);
+		assertTrue(exceped.equals(actual));
+		
+	}
+
+
+}
---
+++ src/Coords/MyCoords.java
@@ -0,0 +1,121 @@
+package Coords;
+
+import Geom.Point3D;
+
+public class MyCoords implements coords_converter{
+
+	private final int RADIUS = 6371000;
+	@Override
+	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
+
+		double x =local_vector_in_meter.x();
+		double y =local_vector_in_meter.y();
+		double z =local_vector_in_meter.z();
+
+		double x_radian = Math.asin(x/RADIUS);
+		double x_degree =Math.toDegrees(x_radian);
+
+		double y_radian = Math.asin(y/RADIUS*Lon_Norm(gps.x()));
+		double y_degree =Math.toDegrees(y_radian);
+
+		double x_new = x_degree+gps.x();
+		double y_new = y_degree+gps.y();
+		double z_new = z+gps.z();
+		Point3D p = new Point3D(x_new,y_new,z_new);
+		return p;
+
+	}
+
+	public double Lon_Norm (double x ) {
+
+		double Lon_Norm = Math.cos(x*Math.PI/180);
+		return Lon_Norm;
+
+	}
+
+	@Override
+	public double distance3d(Point3D gps0, Point3D gps1) {
+
+		double diff_lat = gps1.x()-gps0.x();
+		double diff_lon = gps1.y()-gps0.y();
+
+		double diff_lat_rad =Math.toRadians(diff_lat);
+		double diff_lon_rad =Math.toRadians(diff_lon);
+
+		double lat_meter = toMeterLat(diff_lat_rad);
+		double lon_meter = toMeterLon(gps0,diff_lon_rad);
+
+		double distance = Math.sqrt((lat_meter*lat_meter)+(lon_meter*lon_meter));
+
+
+				return distance;
+	}
+
+	private double toMeterLat (double x ) {
+		double ans;
+		ans = Math.sin(x)*RADIUS;
+		return ans;
+
+	}
+
+	private double toMeterLon (Point3D gps , double y) {
+		double ans;
+		ans = Math.sin(y)*RADIUS*Lon_Norm(gps.x());
+		return ans;
+
+	}
+
+
+	@Override
+	public Point3D vector3D(Point3D gps0, Point3D gps1) {
+		
+		double diff_lat = gps1.x()-gps0.x();
+		double diff_lon = gps1.y()-gps0.y();
+		double diff_alt = gps1.z()-gps0.z();
+
+
+		double diff_lat_rad =Math.toRadians(diff_lat);
+		double diff_lon_rad =Math.toRadians(diff_lon);
+
+		double lat_meter = toMeterLat(diff_lat_rad);
+		double lon_meter = toMeterLon(gps0,diff_lon_rad);
+		
+		Point3D p = new Point3D(lat_meter,lon_meter,diff_alt);
+
+		
+		return p;
+	}
+
+	@Override
+	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
+		double[] arr = new double[3];
+		double dx = gps1.x() - gps0.x();
+		double dy = gps1.y() - gps0.y();
+		double dz = gps1.z() - gps0.z();
+		double dis = distance3d(gps0,gps1);
+		double alph = Math.atan(Math.abs(dy/dx));
+		double azimuth = Integer.MAX_VALUE;
+		
+		if ( dx > 0 && dy >0)
+			   azimuth= alph;
+		else if ( dx >0 && dy<0)
+			azimuth= 360-alph;
+		else if ( dx <0 && dy >0)
+			azimuth= 180-alph;
+		else if ( dx<0 && dy<0)
+			azimuth= 180+alph;
+		
+		arr[0] = azimuth;
+		arr[1] = dz;
+		arr[2] = dis;
+		return arr;
+	}
+
+	@Override
+	public boolean isValid_GPS_Point(Point3D p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
